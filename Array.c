abfe553682c40c641703852aebfaf5fc64879747
82102 <sms7624@gmail.com>
2021-11-17 22:24:48 +0900

    ArrayList

---
+++ Array.c
@@ -0,0 +1,14 @@
+#include "Array.h"
+
+ArrayList *createArrayList(int maxElementCount)
+{
+    ArrayList *al;
+    if (maxElementCount > 0)
+    {
+        if (!(al = (ArrayList *)malloc(sizeof(ArrayList) * (maxElementCount + 1))))
+            return NULL;
+        al->maxElementCount = maxElementCount;
+        al->currentElementCount = 0;
+        al->pElement = (ArrayListNode *)malloc);
+    }
+}
