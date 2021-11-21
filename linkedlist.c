f466fabc5fed5f09230cc0b8942956416c9b6ca8
82102 <sms7624@gmail.com>
2021-11-21 23:55:39 +0900

    LinkedList

--- linkedlist.c
+++ linkedlist.c
@@ -5,11 +5,33 @@
 LinkedList *createLinkedList()
 {
     LinkedList *pList;
-}
-int addLLElement(LinkedList *pList, int position, ListNode element);
-int removeLLElement(LinkedList *pList, int position);
-ListNode *getLLElement(LinkedList *pList, int position);
 
-void clearLinkedList(LinkedList *pList);
-int getLinkedListLength(LinkedList *pList);
-void deleteLinkedList(LinkedList *pList);
+    pList = (LinkedList *)malloc(sizeof(LinkedList));
+    if (!pList)
+        return NULL;
+    return pList;
+}
+
+int addLLElement(LinkedList *pList, int position, ListNode element)
+{
+}
+
+int removeLLElement(LinkedList *pList, int position)
+{
+}
+
+ListNode *getLLElement(LinkedList *pList, int position)
+{
+}
+
+void clearLinkedList(LinkedList *pList)
+{
+}
+
+int getLinkedListLength(LinkedList *pList)
+{
+}
+
+void deleteLinkedList(LinkedList *pList)
+{
+}
