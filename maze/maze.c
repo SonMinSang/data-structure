2448ba4a5e045a16eaba2b5a72f0dae4cd09f563
82102 <sms7624@gmail.com>
2021-12-01 23:19:22 +0900

    maze

---
+++ maze/maze.c
@@ -0,0 +1,14 @@
+#include "maze.h"
+
+void solve_maze(Map map, LinkedStack *pStack, StackNode start, StackNode goal)
+{
+    StackNode next;
+    StackNode *back;
+
+    next.x = start.x;
+    next.y = start.y;
+}
+
+void like_element()
+{
+}
